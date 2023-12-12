24e74a0145e1208ceb43654a39268ffe72900191
Jeremie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2023-12-12 15:33:32 +0000

    Added main

--- grokking_llm/__main__.py
+++ grokking_llm/__main__.py
@@ -5,6 +5,23 @@ Copyright 2023-present Laboratoire d'Informatique de Polytechnique.
 Apache Licence v2.0.
 """
 
+import typer
+from loguru import logger
+
+app = typer.Typer()
+
+
+@app.command()
+def train(config: str):
+    logger.info("Calling `train` command...")
+    logger.warning("TO BE COMPLETED")
+
+
+@app.command()
+def evaluate(config: str):
+    logger.info("Calling `evaluate` command...")
+    logger.warning("TO BE COMPLETED")
+
 
 if __name__ == "__main__":
-    pass
+    app()
