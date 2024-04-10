7be464ce84c2933c2830e34318dcf8acf1606e49
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-04-10 15:24:30 +0200

    Removed p_smi scripts

--- scripts/mistral_ethics/p_smi_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source ~/.bashrc
-cd /gpfswork/rech/yfw/upp42qa/grokking_llm
-conda activate expe
-python -u -m grokking_llm deploy-cpu --config=66nIO9d4XxnsfDfc0jaExw --only-metrics=p_smi --njobs=10 --checkpoint=1200
-python -u -m grokking_llm deploy-cpu --config=66nIO9d4XxnsfDfc0jaExw --only-metrics=p_smi --njobs=10 --checkpoint=15600
