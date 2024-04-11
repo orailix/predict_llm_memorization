12f77bba687be03c4c534c44b606a2dfe96b1d5e
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-04-11 14:21:35 +0200

    Modified number of CPU

--- scripts/mistral_ethics/p_smi_full_dataset.sh
+++ scripts/mistral_ethics/p_smi_full_dataset.sh
@@ -3,4 +3,4 @@
 source ~/.bashrc
 cd /gpfswork/rech/yfw/upp42qa/grokking_llm
 conda activate expe
-python -u -m grokking_llm deploy-cpu --config=66nIO9d4XxnsfDfc0jaExw --checkpoint=1200,15600 --only-metrics=p_smi_on_full_dataset --njobs=40
+python -u -m grokking_llm deploy-cpu --config=66nIO9d4XxnsfDfc0jaExw --checkpoint=1200,15600 --only-metrics=p_smi_on_full_dataset --njobs=10
