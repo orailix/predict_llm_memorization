d50114ce839cff7cc97f99880343bed3630dd0a8
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-04-18 15:30:57 +0200

    Added imports for static metrics

--- grokking_llm/measures_stat/__init__.py
+++ grokking_llm/measures_stat/__init__.py
@@ -3,6 +3,11 @@
 # Copyright 2023-present Laboratoire d'Informatique de Polytechnique.
 # Apache Licence v2.0.
 
+from .counterfactual_memo_static import CounterfactualMemoStatic
+from .counterfactual_simplicity import CounterfactualSimplicity
+from .loss_static import LossStatic
 from .main import run_main_measure_stat
+from .memo_logit_gap_static import MemoLogitGapStatic
+from .memo_mia_static import MemoMembershipStatic
 from .p_smi_static import PSmiStatic
 from .static_metrics_group import StaticMetricsGroup
