a3b90bcc4b1ddaa97912354251cab26d3c9d8f15
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-04-19 16:00:03 +0200

    Added script for logit_gap_std

--- scripts/mistral_ethics/static_metrics.sh
+++ scripts/mistral_ethics/static_metrics.sh
@@ -5,6 +5,7 @@ cd /gpfswork/rech/yfw/upp42qa/grokking_llm
 conda activate expe
 python -u -m grokking_llm measure-stat memo_mia --config=66nIO9d4XxnsfDfc0jaExw --checkpoint=1200,15600 --force-recompute &
 python -u -m grokking_llm measure-stat memo_logit_gap --config=66nIO9d4XxnsfDfc0jaExw --checkpoint=1200,15600 --force-recompute &
+python -u -m grokking_llm measure-stat memo_logit_gap_std --config=66nIO9d4XxnsfDfc0jaExw --checkpoint=1200,15600 --force-recompute &
 python -u -m grokking_llm measure-stat memo_counterfactual --config=66nIO9d4XxnsfDfc0jaExw --checkpoint=1200,15600 --force-recompute &
 python -u -m grokking_llm measure-stat simplicity_counterfactual --config=66nIO9d4XxnsfDfc0jaExw --checkpoint=1200,15600 --force-recompute &
 python -u -m grokking_llm measure-stat loss --config=66nIO9d4XxnsfDfc0jaExw --checkpoint=1200,15600 --force-recompute;
