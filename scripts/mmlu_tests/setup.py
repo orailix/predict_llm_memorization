858d201b0da3ae343a8f48b3f6e6b19319ba2da4
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-04-23 15:36:16 +0200

    Removed setup from slurm scripts

--- scripts/mmlu_tests/setup.py
+++ scripts/mmlu_tests/setup.py
@@ -1,5 +1,7 @@
+from grokking_llm.training import get_dataset, get_model
 from grokking_llm.utils import TrainingCfg
 
+# Configs
 TrainingCfg.autoconfig(
     "/gpfswork/rech/yfw/upp42qa/grokking_llm/scripts/mmlu_tests/gemma_mmlu.json"
 ).get_output_dir()
@@ -9,3 +11,11 @@ TrainingCfg.autoconfig(
 TrainingCfg.autoconfig(
     "/gpfswork/rech/yfw/upp42qa/grokking_llm/scripts/mmlu_tests/mistral_mmlu.json"
 ).get_output_dir()
+
+# Models
+get_model(TrainingCfg(model="gemma"))
+get_model(TrainingCfg(model="llama"))
+get_model(TrainingCfg(model="mistral"))
+
+# Dataset
+get_dataset(TrainingCfg(dataset="mmlu"))
--- scripts/mmlu_tests/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ~/.bashrc
-cd /gpfswork/rech/yfw/upp42qa/grokking_llm
-conda activate expe
-
-# Training
-python /gpfswork/rech/yfw/upp42qa/grokking_llm/scripts/mmlu_tests/setup.py
