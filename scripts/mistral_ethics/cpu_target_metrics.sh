58ddca2f1ab19323d46a3d0aa073ba0247497c3b
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-04-24 09:15:36 +0200

    Updated cpu target scripts

--- scripts/mistral_ethics/cpu_target_metrics.sh
+++ scripts/mistral_ethics/cpu_target_metrics.sh
@@ -8,6 +8,5 @@ python -u -m grokking_llm measure-dyn perf --config=DbFZ_3SZsM2OkESOXcQz2Q &
 python -u -m grokking_llm measure-dyn smi --config=DbFZ_3SZsM2OkESOXcQz2Q &
 python -u -m grokking_llm measure-dyn p_smi --config=DbFZ_3SZsM2OkESOXcQz2Q &
 python -u -m grokking_llm measure-dyn weights --config=DbFZ_3SZsM2OkESOXcQz2Q &
-python -u -m grokking_llm measure-dyn memo_proba_gap --config=DbFZ_3SZsM2OkESOXcQz2Q &
-python -u -m grokking_llm measure-dyn memo_logit_gap --config=DbFZ_3SZsM2OkESOXcQz2Q &
+python -u -m grokking_llm measure-dyn logit_gap --config=DbFZ_3SZsM2OkESOXcQz2Q &
 python -u -m grokking_llm measure-dyn sample_loss --config=DbFZ_3SZsM2OkESOXcQz2Q &
