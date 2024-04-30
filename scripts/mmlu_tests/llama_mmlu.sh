03fd574e45b597918216d6619f6326bc89e1491f
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-04-30 10:54:49 +0200

    Bugfix scripts

--- scripts/mmlu_tests/llama_mmlu.sh
+++ scripts/mmlu_tests/llama_mmlu.sh
@@ -9,13 +9,13 @@ python -u /gpfswork/rech/yfw/upp42qa/grokking_llm/scripts/mmlu_tests/setup.py;
 python -u -m grokking_llm train --config=686OrtVTBGgc__IZ1Gxzgg;
 
 # Basic measures
-python -u -m grokking_llm measure-dyn general --config=686OrtVTBGgc__IZ1Gxzgg ;
-python -u -m grokking_llm measure-dyn perf --config=686OrtVTBGgc__IZ1Gxzgg;
+python -u -m grokking_llm measure-dyn general --config=686OrtVTBGgc__IZ1Gxzgg;
 
 # Forward
 python -u -m grokking_llm measure-dyn forward --config=686OrtVTBGgc__IZ1Gxzgg;
 
 # Other metrics
+python -u -m grokking_llm measure-dyn perf --config=686OrtVTBGgc__IZ1Gxzgg;
 python -u -m grokking_llm measure-dyn smi --config=686OrtVTBGgc__IZ1Gxzgg &
 python -u -m grokking_llm measure-dyn p_smi --config=686OrtVTBGgc__IZ1Gxzgg &
 python -u -m grokking_llm measure-dyn weights --config=686OrtVTBGgc__IZ1Gxzgg &
