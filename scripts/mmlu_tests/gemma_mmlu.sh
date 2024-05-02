f205b97379e7723688cafd7a2f99c867f5eafaff
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-05-02 10:09:28 +0200

    Eddited Gemma test deploy config

--- scripts/mmlu_tests/gemma_mmlu.sh
+++ scripts/mmlu_tests/gemma_mmlu.sh
@@ -6,19 +6,19 @@ conda activate expe
 
 # Training
 python -u /gpfswork/rech/yfw/upp42qa/grokking_llm/scripts/mmlu_tests/setup.py;
-python -u -m grokking_llm train --config=7sLH0svuW_pNh8Uecl5Exg;
+python -u -m grokking_llm train --config=dRfdcYPXxygGJ2kuZfsYew;
 
 # Basic measures
-python -u -m grokking_llm measure-dyn general --config=7sLH0svuW_pNh8Uecl5Exg;
+python -u -m grokking_llm measure-dyn general --config=dRfdcYPXxygGJ2kuZfsYew;
 
 # Forward
-python -u -m grokking_llm measure-dyn forward --config=7sLH0svuW_pNh8Uecl5Exg;
+python -u -m grokking_llm measure-dyn forward --config=dRfdcYPXxygGJ2kuZfsYew;
 
 # Other metrics
-python -u -m grokking_llm measure-dyn perf --config=7sLH0svuW_pNh8Uecl5Exg &
-python -u -m grokking_llm measure-dyn smi --config=7sLH0svuW_pNh8Uecl5Exg &
-python -u -m grokking_llm measure-dyn p_smi --config=7sLH0svuW_pNh8Uecl5Exg &
-python -u -m grokking_llm measure-dyn weights --config=7sLH0svuW_pNh8Uecl5Exg &
-python -u -m grokking_llm measure-dyn logit_gap --config=7sLH0svuW_pNh8Uecl5Exg &
-python -u -m grokking_llm measure-dyn sample_loss --config=7sLH0svuW_pNh8Uecl5Exg;
+python -u -m grokking_llm measure-dyn perf --config=dRfdcYPXxygGJ2kuZfsYew &
+python -u -m grokking_llm measure-dyn smi --config=dRfdcYPXxygGJ2kuZfsYew &
+python -u -m grokking_llm measure-dyn p_smi --config=dRfdcYPXxygGJ2kuZfsYew &
+python -u -m grokking_llm measure-dyn weights --config=dRfdcYPXxygGJ2kuZfsYew &
+python -u -m grokking_llm measure-dyn logit_gap --config=dRfdcYPXxygGJ2kuZfsYew &
+python -u -m grokking_llm measure-dyn sample_loss --config=dRfdcYPXxygGJ2kuZfsYew;
 wait;
