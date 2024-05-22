8587047b1f830c783de1538ecefe3959ee481015
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-05-22 11:14:35 +0100

    Added scripts for GPU P-SMI

---
+++ scripts/mmlu_mistral/07_meta_deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bash /gpfswork/rech/yfw/upp42qa/grokking_llm/scripts/mmlu_mistral/02_prepare_deploy.sh
+
+for ((i=0; i<7; i++))
+do
+sbatch /gpfswork/rech/yfw/upp42qa/grokking_llm/scripts/mmlu_mistral/07_forward_deploy.slurm
+done
