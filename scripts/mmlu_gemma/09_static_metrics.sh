863ac5220ae4c2d2dd29becc73d446214e909042
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-05-28 09:50:09 +0200

    Added checkpoints 3750 and 37500 for MMLU static measures except PSMI

--- scripts/mmlu_gemma/09_static_metrics.sh
+++ scripts/mmlu_gemma/09_static_metrics.sh
@@ -4,11 +4,11 @@ source ~/.bashrc
 cd /gpfswork/rech/yfw/upp42qa/grokking_llm
 conda activate expe
 
-python -u -m grokking_llm measure-stat memo_mia --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500 --force-recompute &
-python -u -m grokking_llm measure-stat memo_logit_gap --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500 --force-recompute &
-python -u -m grokking_llm measure-stat memo_logit_gap_std --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500 --force-recompute &
-python -u -m grokking_llm measure-stat memo_counterfactual --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500 --force-recompute &
-python -u -m grokking_llm measure-stat simplicity_counterfactual --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500 --force-recompute &
-python -u -m grokking_llm measure-stat loss --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500 --force-recompute;
+python -u -m grokking_llm measure-stat memo_mia --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500,3750,37500 --force-recompute &
+python -u -m grokking_llm measure-stat memo_logit_gap --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500,3750,37500 --force-recompute &
+python -u -m grokking_llm measure-stat memo_logit_gap_std --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500,3750,37500 --force-recompute &
+python -u -m grokking_llm measure-stat memo_counterfactual --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500,3750,37500 --force-recompute &
+python -u -m grokking_llm measure-stat simplicity_counterfactual --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500,3750,37500 --force-recompute &
+python -u -m grokking_llm measure-stat loss --config=mD9ImObzoCshT5dpStKiMA --checkpoint=750,1500,3750,37500 --force-recompute;
 
 wait;
