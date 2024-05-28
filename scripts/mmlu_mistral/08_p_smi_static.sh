71c4d3552f4d3b5c17f849d9149d7ce83af6ed14
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-05-28 10:37:59 +0200

    Bugfix P-SMI static

--- scripts/mmlu_mistral/08_p_smi_static.sh
+++ scripts/mmlu_mistral/08_p_smi_static.sh
@@ -3,8 +3,9 @@
 source ~/.bashrc
 cd /gpfswork/rech/yfw/upp42qa/grokking_llm
 conda activate expe
+export OMP_NUM_THREADS=40
 
 # P-SMI dynamic
 bash /gpfswork/rech/yfw/upp42qa/grokking_llm/scripts/mmlu_mistral/02_prepare_deploy.sh
-python -u -m grokking_llm deploy-cpu --config=Z5n7bEDGK4JRT4HyLKSzrw --checkpoint=750,1500 --only-metrics=p_smi_on_full_dataset --njobs=40;
-python -u -m grokking_llm measure-stat p_smi --config=Z5n7bEDGK4JRT4HyLKSzrw --checkpoint=750,1500 --njobs=40 --force-recompute;
+python -u -m grokking_llm deploy-cpu --config=Z5n7bEDGK4JRT4HyLKSzrw --checkpoint=750,1500 --only-metrics=p_smi_on_full_dataset --njobs=15;
+# python -u -m grokking_llm measure-stat p_smi --config=Z5n7bEDGK4JRT4HyLKSzrw --checkpoint=750,1500 --njobs=15 --force-recompute;
