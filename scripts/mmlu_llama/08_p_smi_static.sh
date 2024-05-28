9302ff13c995620b6eec4debcf24300237f76739
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-05-28 11:42:30 +0200

    Final version for P-SMI static

--- scripts/mmlu_llama/08_p_smi_static.sh
+++ scripts/mmlu_llama/08_p_smi_static.sh
@@ -6,6 +6,6 @@ conda activate expe
 export OMP_NUM_THREADS=40
 
 # P-SMI dynamic
-# bash /gpfswork/rech/yfw/upp42qa/grokking_llm/scripts/mmlu_llama/02_prepare_deploy.sh
-# python -u -m grokking_llm deploy-cpu --config=ZfTT7SCYBAzBtjT3_sy3wg --checkpoint=750,1500 --only-metrics=p_smi_on_full_dataset --njobs=15;
+bash /gpfswork/rech/yfw/upp42qa/grokking_llm/scripts/mmlu_llama/02_prepare_deploy.sh
+python -u -m grokking_llm deploy-cpu --config=ZfTT7SCYBAzBtjT3_sy3wg --checkpoint=750,1500 --only-metrics=p_smi_on_full_dataset --njobs=15;
 python -u -m grokking_llm measure-stat p_smi --config=ZfTT7SCYBAzBtjT3_sy3wg --checkpoint=750,1500 --njobs=34 --force-recompute;
