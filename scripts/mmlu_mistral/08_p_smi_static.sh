ba8ed7d2c3a41b4edcdaea93ac51a8cd944806e8
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-05-31 10:19:05 +0200

    Added checkpoint 3750 for P-SMI

--- scripts/mmlu_mistral/08_p_smi_static.sh
+++ scripts/mmlu_mistral/08_p_smi_static.sh
@@ -7,5 +7,5 @@ export OMP_NUM_THREADS=40
 
 # P-SMI dynamic
 bash /gpfswork/rech/yfw/upp42qa/grokking_llm/scripts/mmlu_mistral/02_prepare_deploy.sh
-python -u -m grokking_llm deploy-cpu --config=Z5n7bEDGK4JRT4HyLKSzrw --checkpoint=750,1500 --only-metrics=p_smi_on_full_dataset --njobs=15;
-python -u -m grokking_llm measure-stat p_smi --config=Z5n7bEDGK4JRT4HyLKSzrw --checkpoint=750,1500 --njobs=34 --force-recompute;
+python -u -m grokking_llm deploy-cpu --config=Z5n7bEDGK4JRT4HyLKSzrw --checkpoint=750,1500,3750 --only-metrics=p_smi_on_full_dataset --njobs=15;
+python -u -m grokking_llm measure-stat p_smi --config=Z5n7bEDGK4JRT4HyLKSzrw --checkpoint=750,1500,3750 --njobs=34 --force-recompute;
