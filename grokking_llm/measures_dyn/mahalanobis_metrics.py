26fc94b3eae94c410674652da0ee9a784139b510
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-09-06 10:55:08 +0200

    Made Mahalanobis distance computable without test set

--- grokking_llm/measures_dyn/mahalanobis_metrics.py
+++ grokking_llm/measures_dyn/mahalanobis_metrics.py
@@ -32,10 +32,9 @@ class MahalanobisMetrics(DynamicMetricsGroup):
         logger.debug(
             "Loading dataset to retrieve global IDX of the elements of the random split."
         )
-        ds_full = get_dataset(training_cfg)
-        ds_target = get_random_split(ds_full, training_cfg)
-        self.full_idx_set = set(ds_full["global_index"])
-        self.target_idx_set = set(ds_target["global_index"])
+        ds = get_dataset(training_cfg)
+        ds = get_random_split(ds, training_cfg)
+        self.global_idx = sorted(ds["global_index"])
 
         # All layers ?
         self.all_layers = all_layers
@@ -56,7 +55,7 @@ class MahalanobisMetrics(DynamicMetricsGroup):
 
         result = []
         for layer in self.layers_to_process:
-            for idx in sorted(self.target_idx_set):
+            for idx in sorted(self.global_idx):
                 result.append(f"mahalanobis_{layer}_{idx}")
 
         return result
@@ -69,13 +68,13 @@ class MahalanobisMetrics(DynamicMetricsGroup):
         forward_values_trl = get_forward_values(
             self.training_cfg,
             checkpoint,
-            f"train_trl_on_full_dataset",
+            f"train_trl_on_{self.training_cfg.get_config_id()}",
             enable_compressed=False,
         )
         forward_value_rdl = get_forward_values(
             self.training_cfg,
             checkpoint,
-            f"train_rdl_on_full_dataset",
+            f"train_rdl_on_{self.training_cfg.get_config_id()}",
             enable_compressed=False,
         )
         forward_values_all = ForwardValues.concat(
@@ -88,46 +87,24 @@ class MahalanobisMetrics(DynamicMetricsGroup):
                 f"Incorrect loading of the forward values: {len(forward_values_all)} != {len(self.global_idx)}"
             )
 
-        # ==================== Splitting train/test ====================
-
-        train_pos = []
-        train_idx = []
-        target_pos = []
-        target_idx = []
-        for pos, global_idx in enumerate(forward_values_all.global_index.tolist()):
-
-            if global_idx not in self.target_idx_set:
-                train_pos.append(pos)
-                train_idx.append(global_idx)
-            else:
-                target_pos.append(pos)
-                target_idx.append(global_idx)
-
-        # Convert to torch
-        train_pos = torch.Tensor(train_pos).int()
-        target_pos = torch.Tensor(target_pos).int()
-
         # ==================== Estimating distance ====================
 
         mahalanobis_per_layer_per_idx = collections.defaultdict[dict]
         for layer in list(forward_values_all.mcq_states_per_layer):
 
             # Estimating
-            train_features = forward_values_all.mcq_states_per_layer[layer][
-                train_pos
+            features = forward_values_all.mcq_states_per_layer[
+                layer
             ]  # Shape (num_train, 4096)
-            target_features = forward_values_all.mcq_states_per_layer[layer][
-                target_pos
-            ]  # Shape (num_target, 4096)
-            distances = mahalanobis_distance(train_features, target_features)
+            distances = mahalanobis_distance(features, features)
 
             # Saving
-            for pos, idx, d in zip(target_pos.tolist(), target_idx, distances.tolist()):
-                mahalanobis_per_layer_per_idx[layer][idx] = d
+            for pos, idx in enumerate(forward_values_all.global_index.tolist()):
+                mahalanobis_per_layer_per_idx[layer][idx] = distances[pos].item()
 
         # ==================== Output ====================
 
         result = []
         for layer in self.layers_to_process:
-            for idx in sorted(self.target_idx_set):
+            for idx in sorted(self.global_idx):
                 result.append(mahalanobis_per_layer_per_idx[layer][idx])
