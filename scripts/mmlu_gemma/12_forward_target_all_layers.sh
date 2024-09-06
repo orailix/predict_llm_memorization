41f648fdfeb7d4c2ae5e2c47c42a0856de50cc7e
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-09-06 10:03:22 +0200

    Added checkpoint 0 for forward all layers

--- scripts/mmlu_gemma/12_forward_target_all_layers.sh
+++ scripts/mmlu_gemma/12_forward_target_all_layers.sh
@@ -7,5 +7,5 @@ conda activate expe
 # Measure forward
 python -u -m grokking_llm measure-dyn forward_on_all_layers \
     --config=dRfdcYPXxygGJ2kuZfsYew \
-    --checkpoint=750,1500,2250,3000,3750,7500,11250,15000,18750,22500,26250,30000,33750,37500 \
+    --checkpoint=0,750,1500,2250,3000,3750,7500,11250,15000,18750,22500,26250,30000,33750,37500 \
     --force-recompute;
--- scripts/mmlu_llama/12_forward_target_all_layers.sh
+++ scripts/mmlu_llama/12_forward_target_all_layers.sh
@@ -7,5 +7,5 @@ conda activate expe
 # Measure forward
 python -u -m grokking_llm measure-dyn forward_on_all_layers \
     --config=0K1pZkoAv45RZXOIJl9kFw \
-    --checkpoint=750,1500,2250,3000,3750,7500,11250,15000,18750,22500,26250,30000,33750,37500 \
+    --checkpoint=0,750,1500,2250,3000,3750,7500,11250,15000,18750,22500,26250,30000,33750,37500 \
     --force-recompute;
