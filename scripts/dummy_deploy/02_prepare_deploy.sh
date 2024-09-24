ac87581f11f5129dbf8836b93a3314523e676000
Jérémie Dentan <65963192+DentanJeremie@users.noreply.github.com>
2024-09-24 14:34:03 +0200

    Removed unused scripts from deploy

--- scripts/dummy_deploy/02_prepare_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ~/.bashrc
-cd /lustre/fswork/projects/rech/yfw/upp42qa/grokking_llm
-conda activate expe
-
-# Prepare deploy
-python -u -m grokking_llm deploy-prepare --config=SNFuH5grLjkCFCVDdPB1
--- scripts/dummy_deploy/03_meta_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ((i=0; i<2; i++))
-do
-sbatch /lustre/fswork/projects/rech/yfw/upp42qa/grokking_llm/scripts/dummy_deploy/03_deploy_train.slurm
-done
